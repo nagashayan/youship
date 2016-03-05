1fcc837ead1039fcd0eb6664ab804e5a2f158350
Nagashayan <nagashayan1@gmail.com>
2016-03-05 11:56:31 +0530

    last commit file

--- backend/views/site/vieworder.php
+++ backend/views/site/vieworder.php
@@ -2,52 +2,129 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Lorry App';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    
+    
+    <?php //print_r($order); 
+   // print_r($orderinfo); ?>
+    
+    <div class="row">
+        <div class="col-xs-6"><h4>View Order: #<?= $order->id?></h4></div>
+        <div class="col-xs-6"><h4>Status: <?= ($order->status == 1) ? 'Active' : 'Disabled';?></h4></div>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    
+    <div class="row">
+        <div class="col-xs-12">Title: <?= $order->title ?></div>
+       
+    </div>
+    
+    
+    <div class="row">
+        
+        <div class="col-xs-4">Description: <?= $order->description ?></div>
+        
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-4">Pickup Point: <?= $order->pickuplocation ?></div>
+        <div class="col-xs-4">Delivery Point: <?= $order->deliverylocation ?></div>
+        <div class="col-xs-4">Approx KM's: <?= "43" ?></div>
+    </div>
+    
+    
+    <div class="row">
+        <div class="col-xs-4">Pickup Date: <?= $order->pickupdate1 ?></div>
+        <div class="col-xs-4">Delivery Date: <?= $order->deliverydate1 ?></div>
+        
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-12"><h4>Product Dimensions</h4></div>
+       
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-3">Height: <?= $orderinfo->height ?></div>
+        <div class="col-xs-3">Width: <?= $orderinfo->width ?></div>
+        <div class="col-xs-3">Length: <?= $orderinfo->length ?></div>
+        <div class="col-xs-3">Weight: <?= $orderinfo->weight ?></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-3">Breakable: <?= ($orderinfo->breakable == 1) ? 'Yes' : 'No';?></div>
+        <div class="col-xs-3">Wooden: <?= ($orderinfo->wooden == 1) ? 'Yes' : 'No';?></div>
+        <div class="col-xs-3">Packed: <?= ($orderinfo->packed == 1) ? 'Yes' : 'No';?></div>
+        <div class="col-xs-3">Package Type: <?= $orderinfo->packagetype ?></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-12">Map</div>
+       
+    </div>
+    
+    <?php if($order->offerprice != "" && $order->offerprice > 0){ ?>
+    <div class="row">
+        <div class="col-xs-6">Customer Quote</div>
+        <div class="col-xs-6"><?= $order->offerprice;?></div>
         </div>
-
+    <?php } ?>
+    
+    <?php if(count($quotelog) > 0)
+        foreach($quotelog as $quote){ ?>
+        <div class="row">
+            <div class="col-xs-6"><?= ucfirst($quote->quote_from);?> Quote</div>
+        <div class="col-xs-6"><?= $quote->offer_price;?></div>
+        </div>
+    <?php } ?>
+    
+    <?php if($order->offerprice == "" && $order->offerprice == 0 && count($quotelog) == 0 ){ ?>
+    <div class="row">
+        <form action="<?= BACKENDURL ?>/site/update-quote-log" method="post">
+            <label>Your Quote
+                <input type="hidden" name="Quotelog[order_id]" value="<?= $order->id;?>" />
+                <input type="number" name="Quotelog[offer_price]" />
+                <input type="hidden" name="Quotelog[quote_from]" value="operator"/>
+                <input type='submit' value="Submit"/>
+            </label>
+        </form>
+       
     </div>
+    <?php }
+    else if(($order->offerprice > 0 && count($quotelog) == 0) || (count($quotelog) > 0 && $quotelog[count($quotelog) - 1]->quote_from == 'customer')){ ?>
+    <div class="row">
+        <?php if(count($quotelog) < 4) { ?>
+        <form action="<?= BACKENDURL ?>/site/update-quote-log" method="post">
+            <label>Your Quote
+                <input type="hidden" name="Quotelog[order_id]" value="<?= $order->id;?>" />
+                <input type="number" name="Quotelog[offer_price]" />
+                <input type="hidden" name="Quotelog[quote_from]" value="operator"/>
+                <input type='submit' value="Submit"/>
+            </label>
+        </form>
+        <input type="submit" value="Accept"/>
+        
+        <?php }else{ ?>
+        <input type="submit" value="Accept"/>
+        <input type="submit" value="Reject"/>
+        <?php } ?>
+    </div>
+    <?php } 
+    else { echo 'here';?>
+    <!-- <div class="row">
+        
+         <form action="<?= BACKENDURL ?>/site/update-quote-log" method="post">
+            <label>Your Quote
+                <input type="hidden" name="Quotelog[order_id]" value="<?= $order->id;?>" />
+                <input type="number" name="Quotelog[offer_price]" />
+                <input type="hidden" name="Quotelog[quote_from]" value="operator"/>
+                <input type='submit' value="Submit"/>
+            </label>
+        </form>
+        
+        
+    </div> -->
+    <?php } ?>
 </div>
